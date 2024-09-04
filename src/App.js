7d28c1a9a80b7b8745441d6da02d17474ea1d59b
LokeshReddy180900 <87435175+LokeshReddy180900@users.noreply.github.com>
2024-09-04 10:11:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    10WeeksofCloudOps - TODO LIST
+                    10WeeksofCloudOps - TODO LISTS
                 </Row>
   
                 <hr />
